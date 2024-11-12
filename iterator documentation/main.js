efe3ad4d35fbbdf2f078fbd863750bc657fbf3a0
ZubyDollarz <zubydollarz22@gmail.com>
2024-11-12 04:12:08 +0100

    iterator documentation

---
+++ iterator documentation/main.js
@@ -0,0 +1,35 @@
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+
+console.log(words.some(word => {
+  return word.length < 6;
+}));
+
+// Use filter to create a new array
+const interestingWords = words.filter((word) => {return word.length > 5});
+
+console.log(interestingWords.every((word) => {return word.length > 5}));
+
+
+//Chosing right iterators
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+const longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a single value
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+
+console.log(word)
+
+// Choose a method that will return a new array
+const smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.every(num => num < 0);
+// OR nums.some(num => num < 0);
